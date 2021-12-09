70338b217b550b6b3219e35d5b8f8d40374d2997
Carson Johnson <carson74johnson@gmail.com>
2021-12-09 13:42:39 -0500

    pringing our a string asking the user a question and then reading their input to assign to theMatrix variable

--- madlib.py
+++ madlib.py
@@ -16,6 +16,9 @@ print("Welcome user!");
 print("Let's play a game of madlibs!");
 neo = input("please share with me your name?\n");
 
+print(f"Hello {neo}! are you ready?")
+theMatrix = input("What is something you want to know more about?")
+
 # Init Story
 madlibsStory = (f"{theMatrix} is a {system}, {neo}. That {system} is our {enemy}. But when you're {inside}, you look around, what do you see? {profession[0]}, {profession[1]}, {profession[2]}, {profession[3]}. The very minds of the people we are trying to {save}. But until we do, these people are still a part of that {system} and that makes them our {enemy}. You have to understand, most of these people are not ready to be {unplugged}. And many of them are so {adj[0]}, so hopelessly {adj[0]} on the {system}, that they will {fight} to protect it.")
 
