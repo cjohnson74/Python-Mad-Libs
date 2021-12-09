eed97c8adce24f24f7016352185d3eacfd352138
Carson Johnson <carson74johnson@gmail.com>
2021-12-09 03:58:01 -0500

    created a new string array called adj that holds two adjectives from the story

--- madlib.py
+++ madlib.py
@@ -9,11 +9,12 @@ unplugged = ""
 fight = ""
 
 profession = ["","","",""]
+adj = ["","","",""]
 
 # Get Input From User
 
 # Init Story
-madlibsStory = (f"{theMatrix} is a {system}, {neo}. That {system} is our {enemy}. But when you're {inside}, you look around, what do you see? {profession[0]}, {profession[1]}, {profession[2]}, {profession[3]}. The very minds of the people we are trying to {save}. But until we do, these people are still a part of that {system} and that makes them our {enemy}. You have to understand, most of these people are not ready to be {unplugged}. And many of them are so inured, so hopelessly dependent on the {system}, that they will {fight} to protect it.")
+madlibsStory = (f"{theMatrix} is a {system}, {neo}. That {system} is our {enemy}. But when you're {inside}, you look around, what do you see? {profession[0]}, {profession[1]}, {profession[2]}, {profession[3]}. The very minds of the people we are trying to {save}. But until we do, these people are still a part of that {system} and that makes them our {enemy}. You have to understand, most of these people are not ready to be {unplugged}. And many of them are so {adj[0]}, so hopelessly {adj[0]} on the {system}, that they will {fight} to protect it.")
 
 # Print Story
 print(madlibsStory)
