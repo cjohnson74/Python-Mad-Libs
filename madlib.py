7c530d1acfc7dad8e92e0cb02d0d74cf9c5b885f
Carson Johnson <carson74johnson@gmail.com>
2021-12-09 14:45:59 -0500

    prompting the user to give user input and then assigning it to the fight variable

--- madlib.py
+++ madlib.py
@@ -49,6 +49,9 @@ print(f"Lastly I need 2 dystopian adjectives")
 for i in range(len(adj)):
     adj[i] = input(f"Adjective {i+1} / {len(adj)}:")
 
+# Getting the fight variable
+fight = input(f"& a verb:")
+
 # Init Story
 madlibsStory = (f"{theMatrix} is a {system}, {neo}. That {system} is our {enemy}. But when you're {inside}, you look around, what do you see? {profession[0]}, {profession[1]}, {profession[2]}, {profession[3]}. The very minds of the people we are trying to {save}. But until we do, these people are still a part of that {system} and that makes them our {enemy}. You have to understand, most of these people are not ready to be {unplugged}. And many of them are so {adj[0]}, so hopelessly {adj[0]} on the {system}, that they will {fight} to protect it.")
 
