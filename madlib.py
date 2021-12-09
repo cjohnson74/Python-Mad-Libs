e3d1083801447bdbc49114fff12d3be570f1edfc
Carson Johnson <carson74johnson@gmail.com>
2021-12-09 14:11:18 -0500

    created a for loop to prompt the user to add input and then reading each input and assinging it to the current element

--- madlib.py
+++ madlib.py
@@ -27,6 +27,11 @@ enemy = input(f"Give me an opposing noun to {system}")
 
 inside = input(f"Now give me any relaxing noun (present tense)")
 
+print(f"Okay, now I need 4 professions realting to {system}")
+
+for i in range(len(profession)):
+    profession[i] = input(f"Profession (plural) {i+1} / {len(profession)}")
+
 # Init Story
 madlibsStory = (f"{theMatrix} is a {system}, {neo}. That {system} is our {enemy}. But when you're {inside}, you look around, what do you see? {profession[0]}, {profession[1]}, {profession[2]}, {profession[3]}. The very minds of the people we are trying to {save}. But until we do, these people are still a part of that {system} and that makes them our {enemy}. You have to understand, most of these people are not ready to be {unplugged}. And many of them are so {adj[0]}, so hopelessly {adj[0]} on the {system}, that they will {fight} to protect it.")
 
