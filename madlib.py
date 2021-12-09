925e931662397d87597bc86cd7a11b7854e8f7b3
Carson Johnson <carson74johnson@gmail.com>
2021-12-09 13:38:24 -0500

    added print statements to get the users name and then assign the user input to the neo variable

--- madlib.py
+++ madlib.py
@@ -12,6 +12,9 @@ profession = ["","","",""]
 adj = ["","","",""]
 
 # Get Input From User
+print("Welcome user!");
+print("Let's play a game of madlibs!");
+neo = input("please share with me your name?\n");
 
 # Init Story
 madlibsStory = (f"{theMatrix} is a {system}, {neo}. That {system} is our {enemy}. But when you're {inside}, you look around, what do you see? {profession[0]}, {profession[1]}, {profession[2]}, {profession[3]}. The very minds of the people we are trying to {save}. But until we do, these people are still a part of that {system} and that makes them our {enemy}. You have to understand, most of these people are not ready to be {unplugged}. And many of them are so {adj[0]}, so hopelessly {adj[0]} on the {system}, that they will {fight} to protect it.")
