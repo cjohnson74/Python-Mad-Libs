53c85d6bf54ed891feaacc7f176e367e5d70df24
Carson Johnson <carson74johnson@gmail.com>
2021-12-09 14:03:51 -0500

    used two console inputs one for enemy and inside to get user input and assign to to those variables

--- madlib.py
+++ madlib.py
@@ -23,6 +23,10 @@ print(f"Ooooh! You want to know more about {theMatrix} huh?")
 print(f"Okay well first, tell me what you already know about {theMatrix}")
 system = input(f"What noun would you categorize {theMatrix} as:")
 
+enemy = input(f"Give me an opposing noun to {system}")
+
+inside = input(f"Now give me any relaxing noun (present tense)")
+
 # Init Story
 madlibsStory = (f"{theMatrix} is a {system}, {neo}. That {system} is our {enemy}. But when you're {inside}, you look around, what do you see? {profession[0]}, {profession[1]}, {profession[2]}, {profession[3]}. The very minds of the people we are trying to {save}. But until we do, these people are still a part of that {system} and that makes them our {enemy}. You have to understand, most of these people are not ready to be {unplugged}. And many of them are so {adj[0]}, so hopelessly {adj[0]} on the {system}, that they will {fight} to protect it.")
 
